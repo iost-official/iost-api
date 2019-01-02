14af0d99e630635671213bb4ee4e7e4126babadb
liushiqi9 <liushiqi9@gmail.com>
2019-01-02 20:47:57 +0800

    fix mongo index unique problem

--- config/mongoIndex.js
+++ config/mongoIndex.js
@@ -1,4 +1,4 @@
 db.txs.ensureIndex({"tx.hash":1},{ unique: true })
-db.txs.ensureIndex({"blocknumber":1},{ unique: true })
+db.txs.ensureIndex({"blocknumber":1})
 db.blocks.ensureIndex({"number":1},{ unique: true })
 db.blocks.ensureIndex({"hash":1},{ unique: true })
