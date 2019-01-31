5251a4cc69c3fdb8c0c154ca698041aaa9bc3227
liuziran <1213092802@qq.com>
2019-01-31 14:46:01 +0800

    fix build bug

--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"eosbet-go/config"
-
+	"github.com/iost-official/iost-api/config"
 	"github.com/iost-official/iost-api/controller"
 	"github.com/iost-official/iost-api/middleware"
 	"github.com/labstack/echo"
@@ -10,7 +9,7 @@ import (
 )
 
 func main() {
-	config.ReadConfig()
+	config.ReadConfig("")
 	e := echo.New()
 	e.Debug = true
 	e.HTTPErrorHandler = middleware.CustomHTTPErrorHandler
